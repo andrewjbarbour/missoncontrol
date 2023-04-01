1d0a29d0e80121cf620ae30c32cbd39cba42988e
Andrew Barbour <barbouraj@gmail.com>
2023-04-01 16:58:09 -0700

    update styling

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,8 +3,11 @@ import * as React from "react";
 
 export default function Navbar() {
   return (
-    <div className="w-screen flex-shrink-0">
-      <Link href="/" className="text-2xl">
+    <div className="w-screen flex-shrink-0 p-8">
+      <Link
+        href="/"
+        className="text-3xl hover:underline hover:underline-offset-8 decoration-2"
+      >
         Andrew Barbour
       </Link>
     </div>
