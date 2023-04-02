682d2d89bb380381689abe9bfaf1365013fda0ab
Andrew Barbour <barbouraj@gmail.com>
2023-04-02 15:14:03 -0700

    update contact form transitions

--- app/contact/contact-field.tsx
+++ app/contact/contact-field.tsx
@@ -14,7 +14,7 @@ export default function ContactField(props: ContactFieldProps) {
         <div className="flex flex-col m-5 w-96 indent-1">
           <label htmlFor={label}>{label}</label>
           <input
-            className="p-1 indent-2 border-[#666] border border-solid border-opacity-20 rounded"
+            className="p-1 indent-1 border-[#666] border border-solid border-opacity-20 rounded outline-none focus:ring-1 focus:ring-[#666] transition-shadow ease-in duration-300"
             name={label}
             id={label}
             type={type}
@@ -24,12 +24,15 @@ export default function ContactField(props: ContactFieldProps) {
       );
     case "textarea":
       return (
-        <div className="flex flex-col m-5 w-96 indent-1 resize-none">
-          <label htmlFor={label}>{label}</label>
+        <div className="flex flex-col m-5 w-96 resize-none">
+          <label className="my-1" htmlFor={label}>
+            {label}
+          </label>
           <textarea
-            className="p-1 indent-2 border-[#666] border border-solid border-opacity-20 rounded"
+            className="p-1 indent-1 border-[#666] border border-solid border-opacity-20 rounded outline-none focus:ring-1 focus:ring-[#666] transition-shadow ease-in duration-300"
             name={label}
             id={label}
+            rows={7}
             required={required ?? true}
             maxLength={1000}
           />
