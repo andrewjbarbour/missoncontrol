5e607ee164c317ecd80d9eba24b930e5a8387149
Andrew Barbour <barbouraj@gmail.com>
2023-04-01 18:31:14 -0700

    update navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,15 +1,33 @@
 import Link from "next/link";
 import * as React from "react";
 
+const navPages = ["projects", "media", "about", "contact"];
+
+interface NavlinkProps {
+  name: string;
+}
+
+export function Navlink(props: NavlinkProps) {
+  const {name} = props;
+  return <Link href={`/${name}`}>{name.toLocaleUpperCase()}</Link>;
+}
+
 export default function Navbar() {
   return (
-    <div className="w-screen flex-shrink-0 p-8">
-      <Link
-        href="/"
-        className="text-3xl hover:underline hover:underline-offset-8 decoration-2"
-      >
-        Andrew Barbour
-      </Link>
-    </div>
+    <nav className="flex place-content center w-screen flex-shrink-0 p-8">
+      <div className="w-1/2">
+        <Link
+          href="/"
+          className="text-3xl hover:underline hover:underline-offset-8 decoration-2"
+        >
+          Andrew Barbour
+        </Link>
+      </div>
+      <div className="w-1/2 flex p-2 place-content-evenly">
+        {navPages.map(navPage => (
+          <Navlink key={navPage} name={navPage} />
+        ))}
+      </div>
+    </nav>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Projects() {
   return (
     <div className="flex justify-center">
-      <div>Projects</div>
+      <div>Page Content Here</div>
     </div>
   );
 }
