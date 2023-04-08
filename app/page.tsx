340ac88762554bf23cc8d8ffd70dd1a6408acec6
Andrew Barbour <barbouraj@gmail.com>
2023-04-08 16:00:38 -0700

    refactor landing page to tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,11 @@ import SocialBar from "./components/socialbar";
 export default function Home() {
   return (
     <main className="flex flex-row content-center items-center h-full w-full ">
-      {/* <div className="w-1/3 flex place-content-center"></div> */}
       <div className="flex flex-col items-center content-center w-full">
-        <div className="flex place-content-center">
+        <div className="flex place-content-center h-full">
           <Image
             alt="profile photo"
-            className="rounded-full min-h-260 pr-5"
-            style={{
-              minWidth: "371px",
-              maxWidth: "371px",
-              minHeight: "395px",
-              maxHeight: "395px",
-              scale: ".7",
-              aspectRatio: "39/44",
-            }}
+            className="rounded-full min-h-260 pr-5 scale-75 w-[371px] h-[395px]"
             src={profile}
           />
           <div className="max-w-xl text-xl mb-5">
@@ -29,11 +20,11 @@ export default function Home() {
         responsive web apps and software that people love. Right now, I'm
         developing lightweight data visualization and digital twin apps for the Altair One cloud
         platform.`}</p>
-            <p className="mt-5">{`In my spare time, I enjoy sci fi, gardening, playing with my devilish cat, and hiking with my wife. If you are looking for someone to take on a development project or want to connect, send me a message.`}</p>
+            <p className="mt-5">
+              {`In my spare time, I enjoy sci fi, gardening, playing with my devilish cat, and hiking with my wife. 
+              If you are looking for someone to take on a development project or want to connect, send me a message.`}
+            </p>
           </div>
-          {/* <p className="max-w-xl text-xl">
-            {`In my spare time, I enjoy sci fi, gardening, playing with my devilish cat, and hiking with my wife. If you are looking for someone to take on a development project or want to connect, send me a message.`}
-          </p> */}
         </div>
         <div className="flex justify-center mt-5">
           <MainButton href="/projects" text="REVIEW PROJECTS" />
