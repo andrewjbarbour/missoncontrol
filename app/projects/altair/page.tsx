f69dc509ba22751361226e4bfce9e3cf060a2002
Andrew Barbour <barbouraj@gmail.com>
2023-04-26 09:21:01 -0700

    add projects page

---
+++ app/projects/altair/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import aone from "../../public/images/altairone.png";
+
+export default function Projects() {
+  return <div className="flex justify-center items-center h-full">Altair</div>;
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import aone from "../../public/images/altairone.png";
+import altairlogo from "../../public/images/altair-logo-full.jpg";
+
 export default function Projects() {
   return (
-    <div className="flex justify-center">
-      <div>Page Content Here</div>
+    <div className="flex justify-center items-center h-full">
+      <Link href="/projects/altair">
+        <div className="flex flex-col justify-center items-center -mt-20">
+          <Image
+            className="w-[150px] h-[35px] m-8"
+            alt="Altair logo"
+            src={altairlogo}
+          />
+          <Image className="w-10/12 md:w-3/5" alt="Altair" src={aone} />
+        </div>
+      </Link>
     </div>
   );
 }
